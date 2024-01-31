5922c429d046c4ed845d25b1cfe9c6ec3d7a2103
seoeunhyo <seoeunhyo@a.ut.ac.kr>
2024-01-31 14:52:45 +0900

    숫자 문자열과 영단어

--- Lv.1/숫자 문자열과 영단어.py
+++ Lv.1/숫자 문자열과 영단어.py
@@ -1,32 +1,21 @@
 def solution(s):
-    number_lst = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    number_lst = {'zero':0,'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
     temp_number = ''
     answer = ''
     cnt = 0
-    flag = True
 
-    while cnt <= len(s)-1:
-        if s[cnt].isdigit():
+    while cnt < len(s):
+
+        if (s[cnt].isnumeric()):
             answer += s[cnt]
 
         else:
-            while flag:
-
-                if temp_number in number_lst:
-                    answer += str(number_lst[temp_number])
-                    temp_number = ''
-                    cnt += 1
-                    flag = False
-
-                else:
-                    temp_number += s[cnt]
-                cnt += 1
-
-            cnt += 1
+            temp_number += s[cnt]
+            if temp_number in number_lst:
+                answer += str(number_lst[temp_number])
+                temp_number = ''
         cnt += 1
 
-    return answer
+    return int(answer)
 
-print(solution("one4seveneight"))
-
-
+print(solution("23four5six7"))
